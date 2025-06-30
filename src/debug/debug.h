63554eb7f20ce959d75e00963a3c025566016add
Samuel P. <samsam70000@gmail.com>
2025-06-29 23:44:30 -0400

    added: simple ASSERT and NASSERT macros for debugging purposes

--- src/debug/debug.h
+++ src/debug/debug.h
@@ -1,6 +1,18 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
-int kassert(int exit_code);
+#define ASSERT(EXPECTED, CONDITION) \
+	if (CONDITION != EXPECTED) \
+	{ \
+		print((const char*)__LINE__); \
+		panic("__LINE__ HAS ISSUES!"); \
+	} 
+
+#define NASSERT(NEXPECTED, CONDITION) \
+	if (CONDITION == NEXPECTED) \
+	{ \
+		print(__LINE__); \
+		panic("ISSUES!"); \
+	}
 
 #endif
